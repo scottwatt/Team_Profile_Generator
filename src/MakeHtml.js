13854676ba743e0a4fb5b6e19ba6afdc998c3cce
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-08-23 09:00:50 -0700

    adds css to index.html

--- src/MakeHtml.js
+++ src/MakeHtml.js
@@ -4,7 +4,7 @@ const generateManager = function (manager) {
         <div class="card h-100">
             <div class="card-header bg-primary">
                 <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">content_paste</i>
+                <h4>Manager</h4><i class="material-icons">supervisor_account</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${manager.id}</p>
@@ -23,7 +23,7 @@ const generateEngineer = function (engineer) {
         <div class="card h-100">
             <div class="card-header bg-primary">
                 <h3>${engineer.name}</h3>
-                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+                <h4>Engineer</h4><i class="material-icons">terminal</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${engineer.id}</p>
@@ -42,7 +42,7 @@ const generateIntern = function (intern) {
         <div class="card h-100">
             <div class="card-header bg-primary">
                 <h3>${intern.name}</h3>
-                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+                <h4>Intern</h4><i class="material-icons">school</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${intern.id}</p>
@@ -69,7 +69,7 @@ makeHTML = (data) => {
         if (role === 'Manager') {
             const managerCard = generateManager(employee);
 
-            pageArray.push(managerCard);
+            pageArray.unshift(managerCard);
         }
 
         // call engineer function
